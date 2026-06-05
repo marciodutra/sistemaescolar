bb96603fa2fa3a3c890af5170512991431b674ea
Márcio de Oliveira Dutra <78557074+marciodutra@users.noreply.github.com>
2026-06-05 12:57:28 -0300

    Update auth.js

--- backend/middleware/auth.js
+++ backend/middleware/auth.js
@@ -10,8 +10,7 @@ function auth(req, res, next) {
   const [, token] = authHeader.split(" ");
 
   try {
-    const decoded = jwt.verify(token, "segredo");
-
+    const decoded = jwt.verify(token, process.env.JWT_SECRET);
     req.user = decoded;
     next();
   } catch (err) {
