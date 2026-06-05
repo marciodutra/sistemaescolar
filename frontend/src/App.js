a9e638ac1f39e426525be4ebb9b823ed2f17d510
Márcio de Oliveira Dutra <78557074+marciodutra@users.noreply.github.com>
2026-06-05 13:17:22 -0300

    Update App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,22 +1,24 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 import Login from "./pages/Login";
+import Register from "./pages/Register";
 import Dashboard from "./pages/Dashboard";
 import Alunos from "./pages/Alunos";
 import Professores from "./pages/Professores";
 import Turmas from "./pages/Turmas";
-import Register from "./pages/Register";
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
+
         <Route path="/" element={<Login />} />
         <Route path="/register" element={<Register />} />
         <Route path="/dashboard" element={<Dashboard />} />
         <Route path="/alunos" element={<Alunos />} />
         <Route path="/professores" element={<Professores />} />
         <Route path="/turmas" element={<Turmas />} />
+
       </Routes>
     </BrowserRouter>
   );
