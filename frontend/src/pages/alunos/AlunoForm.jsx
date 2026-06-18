ce05804f335c8c3c81684e3abc204d6ae9055b93
Marcio Dutra <professormarciodutra@gmail.com>
2026-06-18 15:47:27 -0300

    fix professores + build ok

--- frontend/src/pages/alunos/AlunoForm.jsx
+++ frontend/src/pages/alunos/AlunoForm.jsx
@@ -238,7 +238,40 @@ export default function AlunoForm() {
             <input style={styles.input} placeholder="Bairro" value={bairro} onChange={e => setBairro(e.target.value)} />
             <input style={styles.input} placeholder="Cidade" value={cidade} onChange={e => setCidade(e.target.value)} />
 
-            <input style={styles.input} placeholder="Estado" value={estado} onChange={e => setEstado(e.target.value)} />
+            <select
+  style={styles.input}
+  value={estado}
+  onChange={(e) => setEstado(e.target.value)}
+>
+  <option value="">Selecione o Estado</option>
+  <option value="AC">Acre</option>
+  <option value="AL">Alagoas</option>
+  <option value="AP">Amapá</option>
+  <option value="AM">Amazonas</option>
+  <option value="BA">Bahia</option>
+  <option value="CE">Ceará</option>
+  <option value="DF">Distrito Federal</option>
+  <option value="ES">Espírito Santo</option>
+  <option value="GO">Goiás</option>
+  <option value="MA">Maranhão</option>
+  <option value="MT">Mato Grosso</option>
+  <option value="MS">Mato Grosso do Sul</option>
+  <option value="MG">Minas Gerais</option>
+  <option value="PA">Pará</option>
+  <option value="PB">Paraíba</option>
+  <option value="PR">Paraná</option>
+  <option value="PE">Pernambuco</option>
+  <option value="PI">Piauí</option>
+  <option value="RJ">Rio de Janeiro</option>
+  <option value="RN">Rio Grande do Norte</option>
+  <option value="RS">Rio Grande do Sul</option>
+  <option value="RO">Rondônia</option>
+  <option value="RR">Roraima</option>
+  <option value="SC">Santa Catarina</option>
+  <option value="SP">São Paulo</option>
+  <option value="SE">Sergipe</option>
+  <option value="TO">Tocantins</option>
+</select>
             <input style={styles.input} placeholder="CEP" value={cep} onChange={e => setCep(e.target.value)} />
           </div>
 
